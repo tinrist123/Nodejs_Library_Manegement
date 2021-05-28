91009bef6083b95d726014a5e102f089af8be586
tinrist123 <59228434+tinrist123@users.noreply.github.com>
2021-05-28 23:15:03 +0700

    Update 20210514004917-create-staff.js
    change user_name -> staff_name in staff model

--- migrations/20210514004917-create-staff.js
+++ migrations/20210514004917-create-staff.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      user_name: {
+      staff_name: {
         type: Sequelize.STRING,
       },
       email: {
